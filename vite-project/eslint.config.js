911aa86b5cfaa28d22cc32b6b72dd13dd0eefa1a
Star <mmdakane@rocketmail.com>
2025-09-30 20:03:49 +0200

    committing initial code

---
+++ vite-project/eslint.config.js
@@ -0,0 +1,60 @@
+import js from "@eslint/js";
+import globals from "globals";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import reactRecommended from "eslint-plugin-react/configs/recommended.js";
+import reactJSXRuntime from "eslint-plugin-react/configs/jsx-runtime.js";
+import reactHooksPlugin from "eslint-plugin-react-hooks";
+import { defineConfig, globalIgnores } from "eslint/config";
+
+export default defineConfig([
+  globalIgnores(["dist"]),
+  {
+    files: ["**/*.{js,jsx}"],
+    extends: [
+      js.configs.recommended,
+      reactHooks.configs["recommended-latest"],
+      reactRefresh.configs.vite,
+    ],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+      parserOptions: {
+        ecmaVersion: "latest",
+        ecmaFeatures: { jsx: true },
+        sourceType: "module",
+      },
+    },
+    rules: {
+      "no-unused-vars": ["error", { varsIgnorePattern: "^[A-Z_]" }],
+    },
+  },
+]);
+
+export default [
+  js.configs.recommended,
+  {
+    ignores: ["node_modules", "dist"],
+  },
+  {
+    files: ["**/*.{js,jsx}"],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+      },
+    },
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+  },
+  reactRecommended,
+  reactJSXRuntime,
+  {
+    plugins: {
+      "react-hooks": reactHooksPlugin,
+    },
+    rules: reactHooksPlugin.configs.recommended.rules,
+  },
+];
